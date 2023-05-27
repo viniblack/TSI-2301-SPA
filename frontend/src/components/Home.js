a83148dc333095fc70c8db61ef4567229f1d7336
ViniBlack <viniibl4ck@gmail.com>
2023-05-27 15:33:59 -0300

    terminando

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,22 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import TarefaContext from "../TarefaContext";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   const tarefa = useContext(TarefaContext);
+  
+  const handleChange = (event) => {
+    fetch(`http://localhost:3002/atualizar/${event._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nome: event.nome,
+        status: !event.status,
+      }),
+    });
+  };
 
   return tarefa ? (
     <div>
@@ -12,24 +25,24 @@ export default function Home() {
         <Link to="/criar">ADD</Link>
       </button>
       <div>
-        <table>
-          {tarefa.map((tarefa) => {
-            return (
-              <>
-                <input
-
-                  id={tarefa._id}
-                  type="checkbox"
-                ></input>{" "}
-                <label for={tarefa._id}>{tarefa.nome}</label>
-                <button>
-                  <Link to={`/editar/${tarefa._id}`}>Editar</Link>
-                </button>
-                <br />
-              </>
-            );
-          })}
-        </table>
+        {tarefa.map((tarefa) => {
+          return (
+            <div key={tarefa._id}>
+              <input
+                
+                onChange={() => handleChange(tarefa)}
+                id={tarefa._id}
+                type="checkbox"
+                defaultChecked={tarefa.status}
+              ></input>
+              <label  style={tarefa.status === true ? {textDecoration: "line-through"} : {textDecoration: "normal"}} htmlFor={tarefa._id}>{tarefa.nome}</label>
+              <button>
+                <Link to={`/editar/${tarefa._id}`}>Editar</Link>
+              </button>
+              <br />
+            </div>
+          );
+        })}
       </div>
     </div>
   ) : (
